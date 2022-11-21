940311ff32a206c36f4ed73d1594309235cad3ce
Mahboubeh Tabasi <mahboube.tabasi@gmail.com>
2022-11-21 12:27:15 +0330

    adding my files to my branch

---
+++ core/db_manager.py
@@ -0,0 +1,34 @@
+import psycopg2
+from models.mdels import *
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import session
+from sqlalchemy.sql import func
+
+
+engine = create_engine('postgresql://postgres:Mahboube.1@127.0.0.1:5432/resturant')
+base = declarative_base()
+session = session.sessionmaker(bind=engine)()
+
+
+def add(student):
+    session.add(student)
+    session.commit()
+
+def item_count(table_name):
+    return session.query(table_name).count()
+
+def total_price(column):
+    total = session.query(func.sum(column)).scalar()
+    return f'{total} tomans'
+
+# print(total_price(Menu_item.price))
+def bring_data(table_name):
+    return session.query(table_name).all()
+
+# def pic_adrs_chooser(name):
+#     if name == 'pizza':
+#         return '01.png'
+
+
+# print(bring_data(Menu_item))
